fb61e7dc4b227d13c3f53ea17a469c11c3aff3fd
jjbart <josiahjbartlett@gmail.com>
2016-04-26 22:37:45 -0400

    Alter header.php
    change enables date picker and login modal

--- views/header.php
+++ views/header.php
@@ -16,13 +16,13 @@
       <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" /> <!-- for the datepicker -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/> <!-- for the datepicker -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-      <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-      <script src="views/datePicker.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
   <body>
-    
+      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+      <script src="views/datePicker.js"></script>
+      
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <nav class="navbar navbar-default">
       <div class="container-fluid"> 
   
