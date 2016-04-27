31b4f48b020c04ea9141b4f4c622427329c17aec
jjbart <josiahjbartlett@gmail.com>
2016-04-26 22:37:02 -0400

    Altered datepicker.js
    Change formats date to correlate with DB

--- views/datePicker.js
+++ views/datePicker.js
@@ -3,8 +3,14 @@
 
 $(document).ready(function() {
     var date_input=$('input[name="date"]'); //date input has the name "date"
+    var date_input1=$('input[name="date1"]'); //date input has the name "date"
     date_input.datepicker({
-        format: 'mm/dd/yyyy',
+        format: 'yyyy-mm-dd',
+        todayHighlight: true,
+        autoclose: true,
+    })
+    date_input1.datepicker({
+        format: 'yyyy-mm-dd',
         todayHighlight: true,
         autoclose: true,
     })
