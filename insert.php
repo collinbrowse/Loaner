f3125efed3f8b0a70a75b6f1a9f893c3f9a05941
Collin Browse <cbrowse13@gmail.com>
2016-04-30 15:03:51 -0400

    More info for car availability
    Grabbed more info from POST so that a car can be "available" to rent

--- insert.php
+++ insert.php
@@ -21,11 +21,16 @@ else {
   $mileage = trim($_POST['mileage']);
   $rating = null;
   $description = trim($_POST['description']);
+  $username = trim($_SESSION['user_id']);
+  $startRental = trim($_POST['startRental']);
+  $endRental = trim($_POST['endRental']);
+  $location = trim($_POST['location']);
   
   // Create a new model to insert a new car
   require_once('models/car.php');
   $add = new Car($cs332db);
   $add->insert($model, $make, $year, $seats, $mileage, $rating, $description);
+  $result = $add->newCar($username, $startRental, $endRental, $location);
 }
 // Return home
 if (isset($_SESSION['user_id'])) {
