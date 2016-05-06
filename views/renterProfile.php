aac752e90bd2fade7f2763f9e29132dd8065ae48
jjbart <josiahjbartlett@gmail.com>
2016-05-06 13:18:44 -0400

    Altered view renterProfile.php
    Added profile info to page

--- views/renterProfile.php
+++ views/renterProfile.php
@@ -1,4 +1,25 @@
 
-
-<h1>Profile page left blank for now</h1>
+<?php require('views/renterMenu.php'); ?>
+    <div class="col-sm-10">
+        <h3>Renter Profile</h3>
+        <img src="views/pics/avatar.gif" alt="avatar" style="width:150px;height:150px;">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="row mgbt-xs-0">
+              <label class="col-xs-3 control-label">First Name:</label>
+              <div class="col-xs-3 controls"><?php echo htmlentities($_SESSION['firstName'], ENT_QUOTES, 'utf-8'); ?></div>
+              <!-- col-sm-10 --> 
+            </div>
+            <div class="row mgbt-xs-0">
+              <label class="col-xs-3 control-label">Last Name:</label>
+              <div class="col-xs-3 controls"><?php echo htmlentities($_SESSION['lastName'], ENT_QUOTES, 'utf-8'); ?></div>
+              <!-- col-sm-10 --> 
+            </div>
+            <div class="row mgbt-xs-0">
+              <label class="col-xs-3 control-label">Age:</label>
+              <div class="col-xs-3 controls"><?php echo htmlentities($_SESSION['age'], ENT_QUOTES, 'utf-8'); ?></div>
+              <!-- col-sm-10 --> 
+            </div>
+          </div>
+    </div>
     
