7652d0b19b91c8733d381c153eedc6b6df58a898
jjbart <josiahjbartlett@gmail.com>
2016-05-10 17:17:16 -0400

    Added controller return.php
    handles renters returning a car

---
+++ return.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+// ---------------------------
+// 
+// ---------------------------
+require_once('models/database.php');
+$cs332db = databaseConnection();
+
+if (!isset($cs332db)) {
+  // Update this to show an error page
+  echo "error";
+}
+else {
+  // Retrieve values from the POST array
+  $car_id = trim($_POST['car_id']);
+  $status = trim($_POST['status']);
+  // Create a new model to insert a new car
+  require_once('models/car.php');
+  $change = new Car($cs332db);
+  $change->changeStatus($car_id, $status);
+  
+
+}
+// Return home
+
+    header('Location: ./renterHistory.php');
+    exit();
